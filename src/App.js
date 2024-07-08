eb41ac103c3ff706b45afc85673f4766368b7f99
Jose L. Ulloa <84105116+jlulloaa@users.noreply.github.com>
2024-07-07 21:58:13 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={process.env.PUBLIC_URL + '/404.png'} alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Simple webapp to manage utility bills
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <a
+        className="App-link"
+        href="https://github.com/jlulloaa/utilities-website"
+        target="_blank"
+        rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+        GitHub Repository
+      </a>
+
       </header>
+      <div className="wrapper">
+        <Dashboard />
+      </div>
     </div>
   );
 }
---
+++ src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div className="content-wrapper">
+            <section className="content-header">
+                <h1>Dashboard</h1>
+            </section>
+            <section className="content">
+                {/* Add your dashboard widgets and components here */}
+            </section>
+        </div>
+    );
+};
+
+export default Dashboard;
